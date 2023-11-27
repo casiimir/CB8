8d5a261878889011c6bb12009a2b8dedbd8893fc
Cas <73138891+casiimir@users.noreply.github.com>
2023-11-27 21:16:34 +0100

    Lezione 27 Novembre 2023

---
+++ netflix-vanilla/script.js
@@ -0,0 +1,34 @@
+// fetch("https://api.themoviedb.org/3/tv/top_rated", {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGNiNzY2YTllNWM0N2ZkOGMzYzg0ODRlZjVjZTY4OSIsInN1YiI6IjY1MGQ0ZDJhZjkyNTMyMDBhZGUxMTJkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KIhoXnYP6YFHse-oAlJpKUpNxh599De8UFJI3BnxvTE",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// {
+//   "adult": false,
+//   "backdrop_path": "/rkB4LyZHo1NHXFEDHl9vSD9r1lI.jpg",
+//   "genre_ids": [
+//     16,
+//     18,
+//     10765,
+//     10759
+//   ],
+//   "id": 94605,
+//   "origin_country": [
+//     "US"
+//   ],
+//   "original_language": "en",
+//   "original_name": "Arcane",
+//   "overview": "Amid the stark discord of twin cities Piltover and Zaun, two sisters fight on rival sides of a war between magic technologies and clashing convictions.",
+//   "popularity": 106.243,
+//   "poster_path": "/fqldf2t8ztc9aiwn3k6mlX3tvRT.jpg",
+//   "first_air_date": "2021-11-06",
+//   "name": "Arcane",
+//   "vote_average": 8.742,
+//   "vote_count": 3443
+// }
