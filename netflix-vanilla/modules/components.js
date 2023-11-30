02dcb88724a17424548423dc402ac8e8133bd5df
Cas <73138891+casiimir@users.noreply.github.com>
2023-11-30 21:42:31 +0100

    Lezione 30 Novembre 2023

--- netflix-vanilla/modules/components.js
+++ netflix-vanilla/modules/components.js
@@ -1,3 +1,61 @@
+import { httpGET } from "./http.js";
+
+const movieModalElGen = (obj) => {
+  const container = document.createElement("div");
+  const imageEl = document.createElement("img");
+  const infoEl = document.createElement("div");
+
+  const infoHeaderEl = document.createElement("div");
+  const infoHeaderTitleEl = document.createElement("h1");
+  const infoHeaderRatingEl = document.createElement("h3");
+  const infoHeaderDateEl = document.createElement("p");
+  const infoHeaderLangEl = document.createElement("p");
+  const infoHeaderAgeEl = document.createElement("p");
+
+  const infoMainEl = document.createElement("div");
+  const infoMainDescEl = document.createElement("p");
+  const infoMainPopEl = document.createElement("p");
+
+  const closeBtnEl = document.createElement("button");
+
+  container.className = "movie-modal";
+  imageEl.src = `https://image.tmdb.org/t/p/w780${obj.poster_path}`;
+  imageEl.alt = obj.original_name || obj.title;
+  imageEl.className = "movie-modal-img";
+  infoEl.className = "movie-modal-info";
+
+  infoHeaderEl.className = "movie-modal-header";
+  infoHeaderTitleEl.textContent = obj.original_name || obj.title;
+  infoHeaderRatingEl.textContent = Math.floor(obj.vote_average) + " ⭐";
+  infoHeaderDateEl.textContent = obj.first_air_date.split("-")[0];
+  infoHeaderLangEl.textContent = obj.original_language;
+  infoHeaderAgeEl.textContent = obj.adult;
+
+  infoMainEl.className = "movie-modal-main";
+  infoMainDescEl.textContent = obj.overview;
+  infoMainPopEl.textContent =
+    obj.popularity >= 100 ? "POPULAR MOVIE!" : "UNPOPULAR MOVIE";
+
+  closeBtnEl.className = "movie-modal-close-btn";
+  closeBtnEl.textContent = "X";
+
+  infoHeaderEl.append(
+    infoHeaderTitleEl,
+    infoHeaderRatingEl,
+    infoHeaderDateEl,
+    infoHeaderLangEl,
+    infoHeaderAgeEl
+  );
+
+  infoMainEl.append(infoMainDescEl, infoMainPopEl);
+  infoEl.append(infoHeaderEl, infoMainEl);
+  container.append(imageEl, infoEl, closeBtnEl);
+
+  return container;
+};
+
+// SYNC - COMPONENTS
+
 const listsContainerElGen = (title) => {
   const container = document.createElement("div");
   const titleEl = document.createElement("h3");
@@ -21,10 +79,30 @@ const cardElGen = (imageData) => {
   const imageEl = document.createElement("img");
 
   imageEl.className = "card-item";
+  imageEl.id = imageData.id;
   imageEl.src = `https://image.tmdb.org/t/p/w500${imageData.poster_path}`;
   imageEl.alt = imageData.name;
 
   return imageEl;
 };
 
-export { cardElGen, cardsListElGen, listsContainerElGen };
+// ASYNC - COMPONENTS
+
+const asyncListContainerElGen = async (endpoint, title) => {
+  const data = await httpGET(endpoint);
+  const listContainerEl = listsContainerElGen(title);
+  const cardsListEl = cardsListElGen();
+
+  data.results.map((serie) => cardsListEl.append(cardElGen(serie)));
+  listContainerEl.append(cardsListEl);
+
+  return listContainerEl;
+};
+
+export {
+  cardElGen,
+  cardsListElGen,
+  listsContainerElGen,
+  asyncListContainerElGen,
+  movieModalElGen,
+};
--- netflix-vanilla/script.js
+++ netflix-vanilla/script.js
@@ -1,64 +1,34 @@
-import { httpGET } from "./modules/http.js";
 import {
-  cardElGen,
-  cardsListElGen,
-  listsContainerElGen,
+  asyncListContainerElGen,
+  movieModalElGen,
 } from "./modules/components.js";
+import { httpGET } from "./modules/http.js";
 
-const asyncListContainerElGen = async (endpoint, title) => {
-  const data = await httpGET(endpoint);
-  const listContainerEl = listsContainerElGen(title);
-  const cardsListEl = cardsListElGen();
-
-  data.results.map((serie) => cardsListEl.append(cardElGen(serie)));
-  listContainerEl.append(cardsListEl);
-
-  return listContainerEl;
-};
-
+const bodyEl = document.body;
 const mainSectionEl = document.querySelector(".main");
 
-// 1 STEP
-// httpGET("/tv/top_rated").then((data) => {
-//   const listContainerEl = listsContainerElGen("Top Rated");
-//   const cardsListEl = cardsListElGen();
-
-//   data.results.map((serie) => cardsListEl.append(cardElGen(serie)));
-
-//   listContainerEl.append(cardsListEl);
-//   mainSectionEl.append(listContainerEl);
-// });
-
-// httpGET("/tv/popular").then((data) => {
-//   const listContainerEl = listsContainerElGen("Popular");
-//   const cardsListEl = cardsListElGen();
-
-//   data.results.map((serie) => cardsListEl.append(cardElGen(serie)));
-
-//   listContainerEl.append(cardsListEl);
-//   mainSectionEl.append(listContainerEl);
-// });
-
-// 2 STEP
-// asyncListContainerElGen("/movie/upcoming", "Movies - Upcoming").then(
-//   (element) => mainSectionEl.append(element)
-// );
-
-// asyncListContainerElGen("/movie/now_playing", "Movies - Now Playing").then(
-//   (element) => mainSectionEl.append(element)
-// );
-
-// asyncListContainerElGen("/tv/top_rated", "Serie Tv - Top Rated").then(
-//   (element) => mainSectionEl.append(element)
-// );
-// asyncListContainerElGen("/tv/popular", "Serie Tv - Popular").then((element) =>
-//   mainSectionEl.append(element)
-// );
-
-// 3 STEP
 Promise.all([
-  asyncListContainerElGen("/movie/upcoming", "Movies - Upcoming"),
-  asyncListContainerElGen("/movie/now_playing", "Movies - Now Playing"),
+  // asyncListContainerElGen("/movie/upcoming", "Movies - Upcoming"),
+  // asyncListContainerElGen("/movie/now_playing", "Movies - Now Playing"),
   asyncListContainerElGen("/tv/top_rated", "Serie Tv - Top Rated"),
   asyncListContainerElGen("/tv/popular", "Serie Tv - Popular"),
-]).then((elements) => elements.map((el) => mainSectionEl.append(el)));
+]).then((elements) =>
+  elements.map((el) => {
+    mainSectionEl.append(el);
+
+    el.addEventListener("click", (event) => {
+      if (event.target.nodeName === "IMG") {
+        httpGET(`/tv/${event.target.id}`).then((data) => {
+          const movieModalEl = movieModalElGen(data);
+          const movieModalBtnEL = movieModalEl.querySelector("button");
+
+          bodyEl.append(movieModalEl);
+
+          movieModalBtnEL.addEventListener("click", () =>
+            movieModalEl.remove()
+          );
+        });
+      }
+    });
+  })
+);
